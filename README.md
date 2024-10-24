c819477f99eb701bc1145c33e1b012dd303963bf
Bartiam <leswar@yandex.ru>
2024-10-24 19:40:52 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DodgeTheStone
+
+Developed with Unreal Engine 5
---
+++ Source/DodgeTheStone/DodgeTheStone.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DodgeTheStone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DodgeTheStone, "DodgeTheStone" );
